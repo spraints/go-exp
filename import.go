19cd5441ef700acf0cac00d55efc5000ffd1383d
Matt Burke <spraints@gmail.com>
2013-02-13 11:54:43 -0500

    Import a sample changeset.

--- import.go
+++ import.go
@@ -1,28 +1,74 @@
 package main
 
 import (
-  "net/http"
+  "fmt"
+  "io"
+  "log"
   "os/exec"
   "time"
-  "log"
 )
 
+type Writer struct {
+  out chan<- []byte
+}
+
+func (w *Writer) WriteLines(lines ...string) {
+  for i := range lines {
+    io.WriteString(w, lines[i] + "\n")
+  }
+}
+
+func (w *Writer) Write(data []byte) (int, error) {
+  w.out <- data
+  return len(data), nil
+}
+
+
 type Author struct {
-  name string
-  email string
+  Name string
+  Email string
 }
 
 type Change interface {
-  Apply(importData chan string)
+  Apply(writer *Writer)
 }
 
 type Changeset struct {
-  author Author
-  date time.Time
-  changes []Change
+  Author Author
+  Date time.Time
+  Message string
+  Changes []Change
 }
 
-func FastImport(importData <-chan []byte) {
+func FetchChangesets(changesets chan<- *Changeset) {
+  changeset := new(Changeset)
+  changeset.Author.Name = "Matt Burke"
+  changeset.Author.Email = "spraints@gmail.com"
+  changeset.Date = time.Now()
+  changeset.Message = "Hi!"
+  changesets <- changeset
+  close(changesets)
+}
+
+func ConvertChangesets(changesets <-chan *Changeset, importData chan<- []byte) {
+  writer := new(Writer)
+  writer.out = importData
+  for changeset := range changesets {
+    writer.WriteLines("commit refs/heads/import",
+      fmt.Sprintf("committer %s <%s> %d -0000", changeset.Author.Name, changeset.Author.Email, changeset.Date.UTC().Unix()),
+      "data <<END_OF_COMMIT_MESSAGE_FI",
+      changeset.Message,
+      "END_OF_COMMIT_MESSAGE_FI")
+    for i := range changeset.Changes {
+      changeset.Changes[i].Apply(writer)
+    }
+    writer.WriteLines("")
+  }
+  close(importData)
+}
+
+func FastImport(importData <-chan []byte, complete chan<- bool) {
+  // Assume that 'target' is already initialized
   cmd := exec.Command("git", "--git-dir=target", "fast-import")
   stdin, err := cmd.StdinPipe()
   if err != nil {
@@ -35,13 +81,16 @@ func FastImport(importData <-chan []byte) {
   for data := range importData {
     stdin.Write(data)
   }
+  complete <- true
 }
 
 func main() {
   importData := make(chan []byte)
   changesets := make(chan *Changeset)
+  done := make(chan bool)
 
-  go FastImport(importstdin)
-  go ChangesetConverter(changesets, importstdin)
-  go ChangesetPump(changesets)
+  go FastImport(importData, done)
+  go ConvertChangesets(changesets, importData)
+  go FetchChangesets(changesets)
+  <-done
 }
